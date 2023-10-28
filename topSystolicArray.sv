7592822dd8ca05ae13115a2ff11cd0dad2440de3
tesa <tej.sanghavi@nordicsemi.no>
2023-10-28 12:56:47 +0200

    If the input is valid store it.

--- topSystolicArray.sv
+++ topSystolicArray.sv
@@ -14,6 +14,28 @@ module topSystolicArray
   , output var logic                 o_validResult
   );
 
+  // {{{ Store valid inputs
+
+  // a and b are 4x4 matrices where each element is 8-bits wide.
+  logic [3:0][3:0][7:0] a_q, b_q;
+
+  always_ff @(posedge i_clk, posedge i_arst)
+    if (i_arst)
+      a_q <= '0;
+    else if (i_validInput)
+      a_q <= i_a;
+    else
+      a_q <= a_q;
+
+  always_ff @(posedge i_clk, posedge i_arst)
+    if (i_arst)
+      b_q <= '0;
+    else if (i_validInput)
+      b_q <= i_b;
+    else
+      b_q <= b_q;
+
+  // }}} Store valid inputs
 
 
 
