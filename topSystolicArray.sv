0d36a65289efd25d0207fe6784fc08e15299a401
tesa <tej.sanghavi@nordicsemi.no>
2023-11-10 13:42:21 +0100

    No need to register inputs. The row and col matrices can be set-up directly from the inputs.

--- topSystolicArray.sv
+++ topSystolicArray.sv
@@ -14,49 +14,33 @@ module topSystolicArray
   , output var logic                 o_validResult
   );
 
-  // {{{ Store valid inputs
+  // {{{ Set-up row and column matrices
 
-  // a and b are 4x4 matrices where each element is 8-bits wide.
-  logic [3:0][3:0][7:0] a_q, b_q;
-
-  always_ff @(posedge i_clk, posedge i_arst)
-    if (i_arst)
-      a_q <= '0;
-    else if (i_validInput)
-      a_q <= i_a;
-    else
-      a_q <= a_q;
-
-  always_ff @(posedge i_clk, posedge i_arst)
-    if (i_arst)
-      b_q <= '0;
-    else if (i_validInput)
-      b_q <= i_b;
-    else
-      b_q <= b_q;
-
-  // }}} Store valid inputs
-
-  // Note: Registering the inputs is not necessary and the rows and columns can be
-  // set-up directly from the inputs.
-
-  // {{{ Set-up rows and columns
-
-  // The rows are inputs to the PEs in the first column.
-  // The columns are inputs to the PEs in the first row.
+  // The rows are inputs to the i_a port of PEs in the first column.
+  // The columns are inputs to the i_b port of PEs in the first row.
   logic [3:0][6:0][7:0] row_q, col_q;
 
   for (genvar i = 0; i < 4; i++) begin: la_perRowCol
 
-    always_ff @(posedge i_clk)
-      row_q[i] <= ({24'b0, a_q[i]} << i*8);
+    always_ff @(posedge i_clk, posedge i_arst)
+      if (i_arst)
+        row_q[i] <= '0;
+      else if (i_validInput)
+        row_q[i] <= ({24'b0, a_q[i]} << i*8);
+      else
+        row_q[i] <= row_d[i];
 
-    always_ff @(posedge i_clk)
-      col_q[i] <= ({24'b0, b_q[0][i], b_q[1][i], b_q[2][i], b_q[3][i]} << i*8);
+    always_ff @(posedge i_clk, posedge i_arst)
+      if (i_arst)
+        col_q[i] <= '0;
+      else if (i_validInput)
+        col_q[i] <= ({24'b0, b_q[0][i], b_q[1][i], b_q[2][i], b_q[3][i]} << i*8);
+      else
+        row_q[i] <= row_d[i];
 
   end: la_perRowCol
 
-  // }}} Set-up rows and columns
+  // }}} Set-up rows and columns matrices
 
 endmodule
 
