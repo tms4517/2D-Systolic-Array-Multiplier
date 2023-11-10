8c9fbdd1e9a67cdc546a6cd7aee48b7ec8ef1a94
tesa <tej.sanghavi@nordicsemi.no>
2023-11-10 15:33:13 +0100

    Create a separate combinational block to decide what the next state of the row and column matrices are.

--- topSystolicArray.sv
+++ topSystolicArray.sv
@@ -18,25 +18,37 @@ module topSystolicArray
 
   // The rows are inputs to the i_a port of PEs in the first column.
   // The columns are inputs to the i_b port of PEs in the first row.
-  logic [3:0][6:0][7:0] row_q, col_q;
+  logic [3:0][6:0][7:0] row_d, row_q;
+  logic [3:0][6:0][7:0] col_d, col_q;
+
+  // When i_validInput is asserted set up the row and col matrices. Else, right
+  // shift by 1 element (8 bits) to pass the next inputs to the systolic array.
 
   for (genvar i = 0; i < 4; i++) begin: la_perRowCol
 
     always_ff @(posedge i_clk, posedge i_arst)
       if (i_arst)
         row_q[i] <= '0;
-      else if (i_validInput)
-        row_q[i] <= ({24'b0, a_q[i]} << i*8);
       else
         row_q[i] <= row_d[i];
+
+    always_comb
+      if (i_validInput)
+        row_d[i] = {24'b0, a_q[i] << i*8};
+      else
+        row_d[i] = {1'b0, row_d[i] >> 8};
 
     always_ff @(posedge i_clk, posedge i_arst)
       if (i_arst)
         col_q[i] <= '0;
-      else if (i_validInput)
-        col_q[i] <= ({24'b0, b_q[0][i], b_q[1][i], b_q[2][i], b_q[3][i]} << i*8);
       else
-        row_q[i] <= row_d[i];
+        col_q[i] <= col_d[i];
+
+    always_comb
+      if (i_validInput)
+        col_d[i] = {24'b0, b_q[0][i], b_q[1][i], b_q[2][i], b_q[3][i]} << i*8};
+      else
+        col_d[i] = {1'b0, col_d[i] >> 8};
 
   end: la_perRowCol
 
