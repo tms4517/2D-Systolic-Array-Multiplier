8161e72abaaa506fa2c1c7337f60a7573ba41601
Tej Sanghavi <Tej.Sanghavi@amd.com>
2026-07-18 05:38:43 -0400

    Refactor matrix dimension definitions to use MATRIX_N for consistency

--- tb/tb_topSystolicArray.cpp
+++ tb/tb_topSystolicArray.cpp
@@ -13,9 +13,11 @@
 #define RESET_NEG_EDGE 5  // Clk edge number to deassert arst.
 #define VERIF_START_TIME 7
 
-#ifndef N
-#define N 64 // Square matrix dimension. Override via `make all N=<value>`.
+#ifndef MATRIX_N
+#define MATRIX_N 4 // Square matrix dimension. Override via `make all N=<value>`.
 #endif
+
+#define N MATRIX_N
 #define WIDTH 8
 
 // Max value of an element.
